b282dbc9b293ea43302089a0a6951f4be55133f6
Max Woollard <Max.Woollard@roh.org.uk>
2024-06-22 19:11:57 +0100

    initial UI

---
+++ src/ui/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, RefinementList } from 'react-instantsearch-dom';
+import './App.css';
+
+const searchClient = algoliasearch('7JU241Q6VH', 'fbe80fe18f09ceb6a0883b332c1052b0');
+
+const Hit = ({ hit }) => (
+  <div className="hit-item">
+    <div className="hit-item-inner">
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <strong>{hit.name}</strong>
+      </div>
+      <div>{hit.description}</div>
+    </div>
+  </div>
+);
+
+function App() {
+  return (
+    <div className="App">
+      <InstantSearch searchClient={searchClient} indexName="products">
+        <div className="search-box-container">
+          <SearchBox />
+        </div>
+        <div className="search-content">
+          <div className="refinement-list-container">
+            <RefinementList
+              attribute="categories"
+              searchable={false}
+              searchablePlaceholder="Search categories"
+              showMore={true}
+            />
+          </div>
+          <div className="hits-container">
+            <Hits hitComponent={Hit} />
+          </div>
+        </div>
+      </InstantSearch>
+    </div>
+  );
+}
+
+export default App;
